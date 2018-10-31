fb7d91092bcacb4e0ac7b29f3b983bc26a940d19
clarakosi <candrewwani@gmail.com>
2018-10-31 10:55:16 -0400

    modified arrow funcs and removed core-js
    Removed core-js from dependencies
    
    Updated arrow functions that are returning a single value into a one-liner

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ module.exports = {
         module.router = makeRouter(clientConstructor);
         const normalizedPath = path.join(__dirname, 'test', 'functional');
         fs.readdirSync(normalizedPath)
-        .map((file) => {
-            return require(`./test/functional/${file}`);
-        });
+        .map(file => require(`./test/functional/${file}`));
     },
     getTestUtils: () => require('./test/utils/test_utils.js'),
     spec: yaml.safeLoad(fs.readFileSync(`${__dirname}/table.yaml`)),
